75f214fcc7e77023c5e9a336946346e5bfd7cc85
张腾 <zhangteng@91jinrong.com>
2016-06-12 09:30:50 +0800

    poj2188 first

---
+++ POJ2188/POJ2188/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  POJ2188
+//
+//  Created by 张腾 on 16/6/12.
+//  Copyright © 2016年 MockingJ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
