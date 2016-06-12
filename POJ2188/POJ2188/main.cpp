f42c456d2022d48801c2982ceba501de43405e5b
张腾 <zhangteng@91jinrong.com>
2016-06-12 21:17:03 +0800

    POJJ2188 MergeSort

--- POJ2188/POJ2188/main.cpp
+++ POJ2188/POJ2188/main.cpp
@@ -8,8 +8,59 @@
 
 #include <iostream>
 
+
+void MergeSort(int A[], int TmpArray[], int Lpos, int Rpos, int rightEnd)
+{
+    int i, leftEnd, numElements, TmpPos;
+    
+    leftEnd = Rpos - 1;
+    TmpPos = Lpos;
+    numElements = rightEnd - Lpos + 1;
+    
+    while( Lpos <= leftEnd && Rpos <= rightEnd)
+    {
+        if ( A[Lpos] <= A[Rpos])
+            TmpArray[TmpPos++] = A[Lpos++];
+        else
+            TmpArray[TmpPos++] = A[Rpos++];
+        
+        while (Lpos <= leftEnd)
+            TmpArray[TmpPos++] = A[Lpos++];
+        
+        while(Rpos <= rightEnd)
+            TmpArray[TmpPos++] = A[Rpos++];
+        
+        for (i = 0; i < numElements; i++, rightEnd--)
+            A[rightEnd] = TmpArray[rightEnd];
+        
+    }
+}
+
+void MSort(int A[], int TempArray[], int left, int right)
+{
+    int center;
+    
+    if (left < right)
+    {
+        center = ( left + right ) / 2;
+        MSort(A, TempArray, left, center);
+        MSort(A, TempArray, center + 1, right);
+        MergeSort(A, TempArray, left, center + 1, right);
+    }
+}
+
+void mergeSort(int A[], int N)
+{
+    int* tmpArray = NULL;
+    tmpArray = (int*)malloc(N * sizeof(int));
+    if (NULL != tmpArray)
+    {
+        MSort(A, tmpArray, 0, N-1);
+        free(tmpArray);
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
     return 0;
 }
